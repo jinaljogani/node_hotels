a8b86562599afbcf4182de5e9d4b8bee817c7cfe
jinaljogani <lillpluto818@gmail.com>
2024-09-17 13:32:33 +0530

    switched to online server

--- routes/menuItemRoutes.js
+++ routes/menuItemRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const MenuItem = require('./../models/MenuItem');
+const MenuItem = require('./../models/menuItem');
 
 router.post('/',async (req,res)=>{
 
